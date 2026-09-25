2fc816c24585601280bdc976f13e20e6da6852b7
Realm9758 <henningraffi50@gmail.com>
2026-09-25 23:58:15 +0100

    floodtide 1.3.8

--- Casks/floodtide.rb
+++ Casks/floodtide.rb
@@ -1,6 +1,6 @@
 cask "floodtide" do
-  version "1.3.7"
-  sha256 "46b5daa31cd270b34fb31e5c0f1e28aecc23e90804625ba29608c89e3bcc9477"
+  version "1.3.8"
+  sha256 "e03421ead62d39997c08043dc169d8279b15a38842cb6c8057174d98b6ff4a10"
 
   url "https://pajb0isilbwjuxjt.public.blob.vercel-storage.com/floodtide/releases/#{version}/Floodtide-#{version}.dmg"
   name "Floodtide"
